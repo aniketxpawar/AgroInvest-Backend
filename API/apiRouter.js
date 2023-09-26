c0cd1a92ba141e742e87018280aaa4824ccc0db8
Aniket Pawar <aniket@archstore.in>
2023-09-26 17:25:48 +0530

    login and sign up

--- API/apiRouter.js
+++ API/apiRouter.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router();
+const apiController = require('./apiController')
 
 router.get('/',async(req,res,next) => {
     res.send("Hello, AgroInvest");
 })
 
-router.post('/signup',)
+router.post('/signupfarmer',apiController.signupfarmer)
+router.post('/login',apiController.login)
 
 module.exports = router;
