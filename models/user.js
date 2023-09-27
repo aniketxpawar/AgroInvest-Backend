f8792a1e91e0b84ea80d7049245603a56b491709
Aniket Pawar <aniket@archstore.in>
2023-09-27 17:41:34 +0530

    email issue solved

--- models/user.js
+++ models/user.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
     },
     address: {
         type: String,
-        required: true,
+        //required: true,
     },
     phone: {
         type: String,
