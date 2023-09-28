f1e9ae3e4c304794c4af3933f776d16ef7987708
Anchit <anchitnayak2003@gmail.com>
2023-09-28 09:55:48 +0530

    dependancy added

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require('express');
 require('dotenv').config();
 const userRouter = require('./user/userRouter.js');
 const mongoose = require('mongoose');
-
+const cors = require("cors");
 const app = express();
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+)
 
 app.use(express.json());
 
@@ -19,3 +30,4 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.use("/user",userRouter)
 
 app.listen(process.env.PORT,()=>console.log("Server is running at port "+process.env.PORT));
+
